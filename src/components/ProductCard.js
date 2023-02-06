9498f5640227a4ff25ec6b691a1105f72c15a7c4
Rocky Pukar <ckrp208@gmail.com>
2023-02-06 11:48:47 +0545

    2:57:40

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
-  return (
-    <div className='col-3'>
-        
-    </div>
-  )
+    return (
+        <div className='col-3'>
+            <Link to="" className='product-card position-relative'>
+                <div className='wishlist-icon position-absolute'>
+                    <Link to=""><img src='images/wish.svg' alt='wishlist' /></Link>
+                </div>
+                <div className='product-image'>
+                    <img src='images/watch.jpg' alt='product' />
+                </div>
+                <div className='product-details'>
+                    <h6 className='brand'>Havels</h6>
+                    <h5 className='product-title'>Kids headphone bulk 10 pack multi colored for students</h5>
+                    <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        isHalf={true }
+                        edit={false}
+                        activeColor="#ffd700"
+                    />
+                    <p className='price'>$1000.00</p>
+                </div>
+                <div className='action-bar position-absolute'>
+                    <div className='d-flex flex-column gap-15'>
+                        <Link to=""><img src='images/view.svg' alt='view' /></Link>
+                        <Link to=""><img src='images/prodcompare.svg' alt='compare' /></Link>
+                        <Link to=""><img src='images/add-cart.svg' alt='addcart' /></Link>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
 }
 
 export default ProductCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -172,6 +172,20 @@ const Home = () => {
         </div>
       </section>
 
+      <section className='featured-wrapper py-5 home-wrapper-2'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <h3 className='section-heading'>Featured Collection</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
+
       <section className='marquee-wrapper py-5'>
         <div className='container-xxl'>
           <div className='row'>
@@ -205,22 +219,6 @@ const Home = () => {
                 </Marquee>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section className='blog-wrapper py-5 home-wrapper-2'>
-        <div className='container-xxl'>
-          <div className='row'>
-            <div className='col-12'>
-              <h3 className='section-heading'>Featured Collection</h3>
-            </div>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
           </div>
         </div>
       </section>
