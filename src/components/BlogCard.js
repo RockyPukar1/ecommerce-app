73e4677311132979fe008e71a00fc872b59fb192
Rocky Pukar <ckrp208@gmail.com>
2023-02-10 19:38:55 +0545

    5:05:23

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const BlogCard = () => {
+const BlogCard = (props) => {
   return (
-    <div className='col-3'>
+    <div className={`${props.grid === 6 ? "col-6" : "col-3"} mb-3`}>
       <div className='blog-card'>
         <div className='card-image'>
-          <img src='images/blog-1.jpg' className='img-fluid' alt='blog' />
+          <img src='images/blog-1.jpg' className='img-fluid w-100' alt='blog' />
         </div>
         <div className='blog-content'>
           <p className='date'>1 Dec, 2022</p>
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,13 +1,41 @@
 import React from 'react'
+import BlogCard from '../components/BlogCard'
 import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta'
 
 const Blog = () => {
   return (
     <>
-        <Meta title={"Blogs"} />
-        <BreadCrumb title="Blogs" />
-
+      <Meta title={"Blogs"} />
+      <BreadCrumb title="Blogs" />
+      <div className='blog-wrapper home-wrapper-2 py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-3'>
+              <div className='filter-card mb-3'>
+                <h3 className='filter-title'>SFind By Categories</h3>
+                <div>
+                  <ul className='ps-0'>
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className='col-9'>
+              <div className='row'>
+                <BlogCard grid={6} />
+                <BlogCard grid={6} />
+                <BlogCard grid={6} />
+                <BlogCard grid={6} />
+                <BlogCard grid={6} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
