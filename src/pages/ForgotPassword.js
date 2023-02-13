6383b0b3605228daa8637827a95b638df24cd2a8
Rocky Pukar <ckrp208@gmail.com>
2023-02-13 14:33:53 +0545

    6:07:24

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+import { Link } from 'react-router-dom';
 
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
+    <>
+        <Meta title="Forgot Password" />
+        <BreadCrumb title="forgot password" />
+        <div className='login-wrapper py-5 home-wrapper-2'>
+            <div className='container-xxl'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='auth-card'>
+                            <h3 className='text-center mb-3'>Reset Your Password</h3>
+                            <form action='#' className='d-flex flex-column gap-15'>
+                                <div>
+                                    <input className='form-control' placeholder='Email' type="email" name='email' />
+                                </div>
+                                <div>
+                                    <Link to="/forgot-password">Forgot Password</Link>
+                                </div>
+                                <div className='mt-3 d-flex justify-content-center gap-15 align-items-center'>
+                                    <button className='button border-0'>Login</button>
+                                    <Link to="" className='button signup'>Sign Up</Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>'
+    </>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <>
         <Meta title="Login" />
         <BreadCrumb title="login" />
+        <div className='login-wrapper py-5 home-wrapper-2'>
+            <div className='container-xxl'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='auth-card'>
+                            <h3 className='text-center mb-3'>Login</h3>
+                            <form action='#' className='d-flex flex-column gap-15'>
+                                <div>
+                                    <input className='form-control' placeholder='Email' type="email" name='email' />
+                                </div>
+                                <div className='mt-1'>
+                                    <input className='form-control' type="password" placeholder='Password' name='password' />
+                                </div>
+                                <div>
+                                    <Link to="/forgot-password">Forgot Password</Link>
+                                </div>
+                                <div className='mt-3 d-flex justify-content-center gap-15 align-items-center'>
+                                    <button className='button border-0'>Login</button>
+                                    <Link to="" className='button signup'>Sign Up</Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
